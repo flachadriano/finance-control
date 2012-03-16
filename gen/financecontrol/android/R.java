65707b3fabd642da93396a68636511771745b31d
Adriano Flach <flachadriano@gmail.com>
2012-03-16 00:02:00 -0300

    change color and layout components

--- gen/financecontrol/android/R.java
+++ gen/financecontrol/android/R.java
@@ -10,38 +10,49 @@ package financecontrol.android;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int dark_green=0x7f040000;
+        public static final int green=0x7f040002;
+        public static final int grey=0x7f040001;
+        public static final int light_green=0x7f040003;
+        public static final int white=0x7f040004;
+    }
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int balance=0x7f020001;
-        public static final int category_add=0x7f020002;
-        public static final int currency=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int button_save=0x7f020002;
+        public static final int category_add=0x7f020003;
+        public static final int currency=0x7f020004;
+        public static final int edit_text=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int radio_button=0x7f020007;
+        public static final int spinner=0x7f020008;
     }
     public static final class id {
-        public static final int balance=0x7f050010;
-        public static final int balanceCurrency=0x7f050011;
-        public static final int balanceImage=0x7f05000f;
-        public static final int balanceLayout=0x7f05000e;
-        public static final int category=0x7f050002;
-        public static final int categoryAdd=0x7f050008;
-        public static final int categoryLabel=0x7f050001;
-        public static final int categoryLayout=0x7f050007;
-        public static final int categoryTitle=0x7f050000;
-        public static final int description=0x7f05000d;
-        public static final int expense=0x7f050005;
-        public static final int record=0x7f050003;
-        public static final int revenue=0x7f050006;
-        public static final int textView1=0x7f050009;
-        public static final int type=0x7f050004;
-        public static final int value=0x7f05000b;
-        public static final int valueCurrency=0x7f05000c;
-        public static final int valueLayout=0x7f05000a;
+        public static final int balance=0x7f060010;
+        public static final int balanceCurrency=0x7f060011;
+        public static final int balanceImage=0x7f06000f;
+        public static final int balanceLayout=0x7f06000e;
+        public static final int category=0x7f060002;
+        public static final int categoryAdd=0x7f060008;
+        public static final int categoryLabel=0x7f060001;
+        public static final int categoryLayout=0x7f060007;
+        public static final int categoryTitle=0x7f060000;
+        public static final int description=0x7f06000d;
+        public static final int expense=0x7f060005;
+        public static final int record=0x7f060003;
+        public static final int revenue=0x7f060006;
+        public static final int type=0x7f060004;
+        public static final int value=0x7f06000b;
+        public static final int valueCurrency=0x7f06000c;
+        public static final int valueLabel=0x7f060009;
+        public static final int valueLayout=0x7f06000a;
     }
     public static final class layout {
         public static final int category=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
