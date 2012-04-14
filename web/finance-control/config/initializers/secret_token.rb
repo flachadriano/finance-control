859eddf65618219d52bf703254f5a70429926eae
Adriano Flach <flachadriano@gmail.com>
2012-04-14 14:34:40 -0300

    change projects structure, create rails project and readme

---
+++ web/finance-control/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinanceControl::Application.config.secret_token = '0643faedb19ee80814c829bed7ed3203db7780694e6e490fec7bbe49d6ba9872184ae2f51013dbb288b99c10441fbe79f5086887c0bbd6b5952ba2bffaef3888'
