e9939d3fe13189cf3728fbcefd7e323df7906fa3
flachadriano <flachadriano@gmail.com>
2015-11-14 10:59:31 -0200

    creating categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :allow_all_origins
-
-  def allow_all_origins
-  	response.headers['Access-Control-Allow-Origin'] = "*"
-  end
-
+  # protect_from_forgery with: :null_session
+  # before_action :allow_all_origins
+  #
+  # def allow_all_origins
+  # 	response.headers['Access-Control-Allow-Origin'] = "*"
+  # end
 end
